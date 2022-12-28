f385c29691869d0664e87a0b20ee604ed2784965
DluxusBot <dluxusbot@gmail.com>
2022-12-29 06:19:49 +0700

    Dluxus

---
+++ plugins/info-paystore.js
@@ -0,0 +1,67 @@
+let handler = async (m, { conn }) => {
+	//-----PRICE
+//sewa
+let sh = '5'
+let sn = '15'
+let ss = '30'
+let sp = '35'
+let sv = '65'
+//premium
+let ph = '5'
+let pn = '20'
+let pp = '40'
+let pv = '50'
+let ppm = '70'
+let info = `
+╭━━━━「 *SEWA* 」
+┊⫹⫺ *GRATIS UNTUK SELAMANYA*
+╰═┅═━––––––๑
+
+╭━━━━「 *PREMIUM* 」
+┊⫹⫺ *-*
+╰═┅═━––––––๑
+
+*⫹⫺ PAYMENT:*
+• *Pulsa Telkomsel:* [${ppulsa}]
+• *Dana:* [${pdana}]
+• *Gopay:* [${pgopay}]
+• *Ovo:* [${povo}]
+• *Link Aja:* [${plinkaja}]`
+const sections = [
+   {
+	title: `${htjava} SEWA ✦-------`,
+	rows: [
+	    {title: "🔖 HEMAT", rowId: '.order *Paket:* HEMAT • Sewa', description: 'PRICE: ' + sh + 'k (1 minggu)' },
+	    {title: "🔖 NORMAL", rowId: '.order *Paket:* NORMAL • Sewa', description: 'PRICE: ' + sn + 'k (1 bulan)' },
+	{title: "🔖 STANDAR", rowId: '.order *Paket:* STANDAR • Sewa', description: 'PRICE: ' + ss + 'k (2 bulan)' },
+	{title: "🔖 PRO", rowId: '.order *Paket:* PRO • Sewa', description: 'PRICE: ' + sp + 'k (4 bulan)' },
+	{title: "🔖 VIP", rowId: '.order *Paket:* VIP • Sewa', description: 'PRICE: ' + sv + 'k (12 bulan)' },
+	]
+    }, {
+    title: `${htjava} PREMIUM ✦-------`,
+	rows: [
+	    {title: "🌟 HEMAT", rowId: '.order *Paket:* HEMAT • Premium', description: 'PRICE: ' + ph + 'k (1 minggu)' },
+	    {title: "🌟 NORMAL", rowId: '.order *Paket:* NORMAL • Premium', description: 'PRICE: ' + pn + 'k (1 bulan)' },
+	{title: "🌟 PRO", rowId: '.order *Paket:* PRO • Premium', description: 'PRICE: ' + pp + 'k (4 bulan)' },
+	{title: "🌟 VIP", rowId: '.order *Paket:* VIP • Premium', description: 'PRICE: ' + pv + 'k (8 bulan)' },
+	{title: "🌟 PERMANENT (jual script bot)", rowId: '.order *Paket:* PERMANENT • Premium (jual script bot)', description: 'PRICE: ' + ppm + 'k (UNLIMITED)' },
+	]
+    },
+]
+
+const listMessage = {
+  text: info,
+  footer: botdate,
+  title: wm,
+  buttonText: "Click Here!",
+  sections
+}
+await conn.sendMessage(m.chat, listMessage)
+//conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
+}
+
+handler.help = ['sewa', 'premium']
+handler.tags = ['main']
+handler.command = /^(sewa(bot)?|premium)$/i
+
+export default handler
